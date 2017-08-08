3f395cd49d458049c3182c888a1af3081c36ceb4
sigitamocevice <sigita.z@gmail.com>
2017-08-08 14:00:11 +0300

    validation ir property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  question = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
